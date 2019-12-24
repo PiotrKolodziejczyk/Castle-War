db80d6f991e9c7459a6893568e086b7505c9b79e
Piotr Kolodziejczyk <kolek2010@gmail.com>
2019-12-24 15:11:33 +0100

    Add knight photo and fix bug with Tower Position

--- Castle War/Assets/Scripts/BattleSceneScripts/TowerManager.cs
+++ Castle War/Assets/Scripts/BattleSceneScripts/TowerManager.cs
@@ -6,15 +6,15 @@ public class TowerManager : MonoBehaviour
     public GameObject towerPrefab;
     public GameObject towerPrefab1;
     public GameObject towerPrefab2;
-    private GameObject tower;
-    private bool isDraggingTower = false;
+    GameObject tower;
+    bool isDraggingTower = false;
     internal bool building;
-    public MeshRenderer[] meshes;
+    MeshRenderer[] meshes;
     internal bool mightBuilding = true;
     public Collider[] hitColliders;
     Regex regex;
-
     float y;
+
     private void Awake()
     {
         regex = new Regex(@"Tower[ABC]\(Clone\)");
@@ -22,32 +22,20 @@ public class TowerManager : MonoBehaviour
         {
             meshes = GetComponentsInChildren<MeshRenderer>();
         }
-        if (transform.tag != "Zero")
-        {
-            y = 0;
-        }
-        else
-        {
-            y = 16;
-        }
-        Debug.Log(transform.name + y);
     }
 
     private void Update()
     {
-
-        
-
         if (isDraggingTower)
         {
             MoveTowerToMouse();
-            if (Input.GetMouseButtonDown(0) && tower.GetComponent<TowerManager>().building&& tower.GetComponent<TowerManager>().mightBuilding)
+            if (Input.GetMouseButtonDown(0) && tower.GetComponent<TowerManager>().building && tower.GetComponent<TowerManager>().mightBuilding)
             {
                 StopDragging();
 
             }
         }
-        if (GetComponent<TowerManager>().building && GetComponent<TowerManager>().mightBuilding&&regex.IsMatch(GetComponent<TowerManager>().name))
+        if (GetComponent<TowerManager>().building && GetComponent<TowerManager>().mightBuilding && regex.IsMatch(GetComponent<TowerManager>().name))
         {
             var meshList = GetComponentsInChildren<MeshRenderer>();
             for (int i = 0; i < meshList.Length; i++)
@@ -55,7 +43,7 @@ public class TowerManager : MonoBehaviour
                 meshList[i].material.color = Color.white;
             }
         }
-        else if(regex.IsMatch(GetComponent<TowerManager>().name))
+        else if (regex.IsMatch(GetComponent<TowerManager>().name))
         {
             var meshList = GetComponentsInChildren<MeshRenderer>();
             for (int i = 0; i < meshList.Length; i++)
@@ -63,7 +51,6 @@ public class TowerManager : MonoBehaviour
                 meshList[i].material.color = Color.red;
             }
         }
-
     }
 
     private void StopDragging()
@@ -74,11 +61,8 @@ public class TowerManager : MonoBehaviour
         }
         isDraggingTower = false;
         Cursor.visible = true;
-
-
         tower.GetComponent<BoxCollider>().size = new Vector3(4, 2, 4);
         tower.gameObject.name = "Tower";
-
     }
 
     private void MoveTowerToMouse()
@@ -89,6 +73,7 @@ public class TowerManager : MonoBehaviour
 
     public void InstantiateTowerB()
     {
+        y = 16;
         tower = Instantiate(towerPrefab, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0));
         isDraggingTower = true;
         Cursor.visible = false;
@@ -99,6 +84,7 @@ public class TowerManager : MonoBehaviour
     }
     public void InstantiateTowerA()
     {
+        y = 16;
         tower = Instantiate(towerPrefab1, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0));
         isDraggingTower = true;
         Cursor.visible = false;
@@ -109,6 +95,7 @@ public class TowerManager : MonoBehaviour
     }
     public void InstantiateTowerC()
     {
+        y = 0;
         tower = Instantiate(towerPrefab2, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0));
         isDraggingTower = true;
         Cursor.visible = false;
@@ -124,32 +111,19 @@ public class TowerManager : MonoBehaviour
         mousePoint.z = 250;
         return Camera.main.ScreenToWorldPoint(mousePoint);
     }
+
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.name == "Tower" && regex.IsMatch(GetComponent<TowerManager>().name))
         {
-            //var meshList = GetComponentsInChildren<MeshRenderer>();
-            //for (int i = 0; i < meshList.Length; i++)
-            //{
-            //    meshList[i].material.color = Color.red;
-            //}
             GetComponent<TowerManager>().mightBuilding = false;
         }
-
-
-
-
     }
+
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.name == "Tower" )
+        if (other.gameObject.name == "Tower")
         {
-
-            //var meshList = GetComponentsInChildren<MeshRenderer>();
-            //for (int i = 0; i < meshList.Length; i++)
-            //{
-            //    meshList[i].material.color = Color.white;
-            //}
             GetComponent<TowerManager>().mightBuilding = true;
         }
     }
