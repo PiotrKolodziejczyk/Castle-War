8232aa12bb88d3dc805b3a2ad956a9c5dffcab50
Piotr Kolodziejczyk <kolek2010@gmail.com>
2020-01-22 21:18:03 +0100

    add sea sounds and horse sounds

---
+++ Castle War/Assets/SeaSounds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SeaSounds : MonoBehaviour
+{
+    public AudioSource audio;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Player")
+        audio.Play();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            audio.Stop();
+    }
+}
